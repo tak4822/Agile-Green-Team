82b4e3b8274d0a7c80de681f623a037d2c4ca9f5
c4rlz <carly.ewasiuk94@gmail.com>
2017-09-14 21:10:25 -0700

    Update file structure

--- js/script.js
+++ js/script.js
@@ -1,3 +1,30 @@
+
 $(function(){
-    
+    // $('img').click(function(){
+
+        $('#btn').click(function(){
+            $.getJSON("js/government-party.json", function(data) {
+                console.log(data);
+                console.log('helo');
+            });
+        // })
+        // $.getJSON('government-party.json', function(data) {
+        //     console.log(data);
+        });
+
+    //  });
+
+    // $.ajax({
+    //     url: 'parties.json',
+    //     dataType: 'json',
+    //     type: 'get',
+    //     cache: true,
+    //     success: function(){
+    //         console.log(data);
+    //     }
+
+    // });
+
 })
+
+// })
--- parties.php
+++ parties.php
@@ -1,8 +1,10 @@
+
 <?php include "partials/head.php" ?>
 
 
 
 <body>
   <h1> Canada's Political Parties </h1>
+  <button id='btn'> CLick me </button>
 
 <?php include "partials/footer.php" ?>
