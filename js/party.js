90ced7b0d4ac929058147eaa921ebdea7678f815
c4rlz <carly.ewasiuk94@gmail.com>
2017-09-17 18:18:14 -0700

    Set up party page content with ajax

--- js/party.js
+++ js/party.js
@@ -3,33 +3,67 @@ $(function(){
 
 
         //FETCH PARTY INFORMATION
+        var myAjax = $.ajax({
+            type: 'GET',
+            url: 'js/government-party.json',
+            dataType: 'json',
+            success : function(data){
+                console.log(data);
+                return data;
+            }
+        })
+
+    
         $('.partyContainer__img').click(function(){
-            currentObj = $(this).data('object-name');
+            var objIndex = $(this).data('index');
+            console.log(objIndex);
+
+        
+            
+        //FETCH PARTY INFORMATION
+        var myAjax = $.ajax({
+            type: 'GET',
+            url: 'js/government-party.json',
+            dataType: 'json',
+            success : function(data){
+                console.log(data);
+                myAjaxObj = data; 
+                
+                switch(objIndex){
+                    case 0 : 
+                        var curObj = data.greenPartyObj;
+                        break;
+
+                    case 1: 
+                        var curObj = data.liberalPartyObj;
+                        break;
+
+                    case 2:
+                        var curObj = data.ndpPartyObj;
+                        break;
+
+                    case 3: 
+                        var curObj = data.conservativePartyObj;
+                        break;
+
+                }
+                displayInfo(curObj);
+            }
+        })
+
+
+        
+        //DISPLAY ON PAGE
+        function displayInfo (cur){
+            $('#history').html(
+                "<h2> History of " + cur.name + "</h1> <p>" + cur.history + "<p>" );
+            $('#policies').html(
+                "<h2> Prominent policies of the " + cur.name + "</h2>");
+        }
+
+        
+            // currentObj = $(this).data('object-name');
             // console.log(currentObj);
-
-            var json = (function () {
-                var json = null;
-                $.ajax({
-                    'async': false,
-                    'global': false,
-                    'url': 'js/government-party.json',
-                    'dataType': "json",
-                    'success': function (data) {
-                        json = data;
-                    }
-                });
-                return json;
-            })(); 
-
-
-            // $.getJSON("js/government-party.json", function(data) {
-            //     var myJson = data;
-
-            //     console.log(myJson.currentObj.name);
-
-
-            // });
-
 
 })
 })
--- parties.php
+++ parties.php
@@ -8,33 +8,50 @@
   <button id='btn'> Click me </button>
   <section class='partyContainer'>
     <article class='partyContainer__item'>
-    <img src='img/fff.png' class='partyContainer__img'  data-object-name='liberalPartyObj'>
-
+    <img src='img/fff.png' class='partyContainer__img' data-index=0>
       <div class='partyContainer__img--overlay'>
-
+        <!-- <h2> Liberal Party </h2> -->
       </div>
     </article>
     <article class='partyContainer__item'>
-      <img src='img/fff.png' class='partyContainer__img' data-object-name='conservativePartyObj'>
+      <img src='img/fff.png' class='partyContainer__img' data-index=1>
       <div class='partyContainer__img--overlay'>
-
+        <!-- <h2> Conservative Party </h2> -->
       </div>
     </article>
     <article class='partyContainer__item'>
-      <img src='img/fff.png' class='partyContainer__img' data-object-name='greenPartyObj'>
+      <img src='img/fff.png' class='partyContainer__img' data-index=2>
       <div class='partyContainer__img--overlay'>
-        
+         <!-- <h2> Green Party </h2> -->
       </div>
       </div>
     </article>
     <article class='partyContainer__item'>
-      <img src='img/fff.png' class='partyContainer__img' data-object-name='ndpPartyObj'>
+      <img src='img/fff.png' class='partyContainer__img' data-index=3>
         <div class='partyContainer__img--overlay'>
+        <!-- <h2> New Democratic Party</h2> -->
+
 
         </div>
       </div>
     </article>
 
+  </section>
+
+  <section class='partyInfo'>
+    <article class="partyInfo__item" id='history'>
+      <h2>The future of Canada depends on your vote</h2>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem, sed tenetur harum quis totam excepturi porro qui beatae officia, voluptate unde accusamus delectus, tempore dolore odit laborum facilis quaerat velit? </p>
+    
+    </article>
+  </section>
+
+  <section class='partyInfo'>
+    <article class="partyInfo__item" id='policies'>
+      <h2>Educate yourself and vote based on what matters to you</h2>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem, sed tenetur harum quis totam excepturi porro qui beatae officia, voluptate unde accusamus delectus, tempore dolore odit laborum facilis quaerat velit? </p>
+    
+    </article>
   </section>
 
 <?php include "partials/footer.php" ?>
