494490e4d0657f7dc1b59565efbc5c2b14468114
tak4822 <tkc4822@gmail.com>
2017-09-19 13:20:20 -0700

    ajax with mps

--- js/mp.js
+++ js/mp.js
@@ -6,3 +6,37 @@ for(var i=0; i<province.length; i+=1) {
     alert(this.getAttribute('id'));
   });
 }
+
+$.ajax({
+	url: 'js/mp.json',
+	dataType: 'json',
+	data: {name: 'mps'},
+	success: function(data){
+		var dataArray = data.mps;
+
+		$.each(dataArray, function(i){
+			var whichParty = dataArray[i].caucusShortName;
+      var mpHtml = '<li class="mpList__name">'+ dataArray[i].firstName +' '+ dataArray[i].lastName +' '+ dataArray[i].provTerrName +'</li>';
+
+      switch (whichParty) {
+        case "Liberal":
+          $(".leberal").find(".mpList__list").append(mpHtml);
+
+          break;
+        case "NDP":
+          $(".ndp").find(".mpList__list").append(mpHtml);
+          break;
+        case "Conservative":
+          $(".conservative").find(".mpList__list").append(mpHtml);
+          break;
+        case "Green Party":
+          $(".green").find(".mpList__list").append(mpHtml);
+          break;
+        default:
+          console.log("no party");
+      }
+
+
+		});
+	}
+});
--- mp.php
+++ mp.php
@@ -5,55 +5,32 @@
     <?php include "partials/svg.php" ?>
   </div>
   <div class="mpList__container">
-    <div class="mpList__party">
+
+    <div class="mpList__party leberal">
       <h3 class="mpList__head">Liberal</h3>
       <ul class="mpList__list">
-        <li class="mpList__name">MP 1</li>
-        <li class="mpList__name">MP 2</li>
-        <li class="mpList__name">MP 3</li>
-        <li class="mpList__name">MP 4</li>
-        <li class="mpList__name">MP 5</li>
-        <li class="mpList__name">MP 6</li>
-        <li class="mpList__name">MP 7</li>
+    
       </ul>
     </div>
 
-    <div class="mpList__party">
+    <div class="mpList__party ndp">
       <h3 class="mpList__head">NDP</h3>
       <ul class="mpList__list">
-        <li class="mpList__name">MP 1</li>
-        <li class="mpList__name">MP 2</li>
-        <li class="mpList__name">MP 3</li>
-        <li class="mpList__name">MP 4</li>
-        <li class="mpList__name">MP 5</li>
-        <li class="mpList__name">MP 6</li>
-        <li class="mpList__name">MP 7</li>
+
       </ul>
     </div>
 
-    <div class="mpList__party">
+    <div class="mpList__party conservative">
       <h3 class="mpList__head">concervative</h3>
       <ul class="mpList__list">
-        <li class="mpList__name">MP 1</li>
-        <li class="mpList__name">MP 2</li>
-        <li class="mpList__name">MP 3</li>
-        <li class="mpList__name">MP 4</li>
-        <li class="mpList__name">MP 5</li>
-        <li class="mpList__name">MP 6</li>
-        <li class="mpList__name">MP 7</li>
+
       </ul>
     </div>
 
-    <div class="mpList__party">
+    <div class="mpList__party green">
       <h3 class="mpList__head">Green</h3>
       <ul class="mpList__list">
-        <li class="mpList__name">MP 1</li>
-        <li class="mpList__name">MP 2</li>
-        <li class="mpList__name">MP 3</li>
-        <li class="mpList__name">MP 4</li>
-        <li class="mpList__name">MP 5</li>
-        <li class="mpList__name">MP 6</li>
-        <li class="mpList__name">MP 7</li>
+
       </ul>
     </div>
   </div>
