1f0bf1ba4e99a6d6ccbd2d1e3f74df3c558ede9c
c4rlz <carly.ewasiuk94@gmail.com>
2017-09-18 21:50:21 -0700

    start coding footer

--- partials/footer.php
+++ partials/footer.php
@@ -1,14 +1,35 @@
-<footer>
-  <h3> Keep up to date, Subscribe to our newsletter </h3>
-  <form action="<?php echo htmlentities($_SERVER['PHP_SELF'])?>" method='POST'>
-    <label for='nameInp'> Full Name 
-    <input type='text' name='fullName' id='nameInp' class='form-input'>
-    </label>
-    <label for='nameInp'> Email Address
-    <input type='email' name='email' id='emailInp' class='form-input'>
-    </label>
-    <input type='submit' value='Subscribe'>
-  </form>
+<footer class='footerContainer'>
+  <section class='footerContainer__links'>
+    <h3> Contact Us </h3>
+    <p> vanarts@vanarts.com </p>
+    <p> 604-509-2093 </p>
+
+    <div class='footerContainer__nav'>
+         <a href="index.php" id='home'>Home
+        </a>
+        <a href="parties.php" id='parties'>Parties
+        </a>
+          <a href="services.php"  id='mp'>Who's My MP</a>
+          <a href="rates.php"  id='compare'>Comparing Parties</a>
+
+    </div>
+  </section>
+
+
+
+  </section>
+  <section class='footerContainer__form'>
+    <h3> Keep up to date, Subscribe to our newsletter </h3>
+    <form action="<?php echo htmlentities($_SERVER['PHP_SELF'])?>" method='POST'>
+      <label for='nameInp'> Full Name 
+      <input type='text' name='fullName' id='nameInp' class='form-input'>
+      </label>
+      <label for='nameInp'> Email Address
+      <input type='email' name='email' id='emailInp' class='form-input'>
+      </label>
+      <input type='submit' value='Subscribe'>
+    </form>
+</section> 
 </footer>
 <script
   src="https://code.jquery.com/jquery-3.2.1.min.js"
--- parties.php
+++ parties.php
@@ -4,55 +4,53 @@
 
 
 
-<body>
-  <h1> Canada's Political Parties </h1>
-  <button id='btn'> Click me </button>
-  <section class='partyContainer'>
-    <article class='partyContainer__item'>
-    <img src='img/fff.png' class='partyContainer__img' data-index=0>
-      <div class='partyContainer__img--overlay'>
-        <!-- <h2> Liberal Party </h2> -->
-      </div>
-    </article>
-    <article class='partyContainer__item'>
-      <img src='img/fff.png' class='partyContainer__img' data-index=1>
-      <div class='partyContainer__img--overlay'>
-        <!-- <h2> Conservative Party </h2> -->
-      </div>
-    </article>
-    <article class='partyContainer__item'>
-      <img src='img/fff.png' class='partyContainer__img' data-index=2>
-      <div class='partyContainer__img--overlay'>
-         <!-- <h2> Green Party </h2> -->
-      </div>
-      </div>
-    </article>
-    <article class='partyContainer__item'>
-      <img src='img/fff.png' class='partyContainer__img' data-index=3>
+<body id='main'>
+  <main class='partyPageContainer'>
+    <section class='partyContainer'>
+      <article class='partyContainer__item'>
+      <img src='img/fff.png' class='partyContainer__img' data-index=0>
         <div class='partyContainer__img--overlay'>
-        <!-- <h2> New Democratic Party</h2> -->
-
-
+          <!-- <h2> Liberal Party </h2> -->
         </div>
-      </div>
-    </article>
+      </article>
+      <article class='partyContainer__item'>
+        <img src='img/fff.png' class='partyContainer__img' data-index=1>
+        <div class='partyContainer__img--overlay'>
+          <!-- <h2> Conservative Party </h2> -->
+        </div>
+      </article>
+      <article class='partyContainer__item'>
+        <img src='img/fff.png' class='partyContainer__img' data-index=2>
+        <div class='partyContainer__img--overlay'>
+          <!-- <h2> Green Party </h2> -->
+        </div>
+        </div>
+      </article>
+      <article class='partyContainer__item'>
+        <img src='img/fff.png' class='partyContainer__img' data-index=3>
+          <div class='partyContainer__img--overlay'>
+          <!-- <h2> New Democratic Party</h2> -->
 
-  </section>
 
-  <section class='partyInfo'>
-    <article class="partyInfo__item" id='history'>
-      <h2>The future of Canada depends on your vote</h2>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem, sed tenetur harum quis totam excepturi porro qui beatae officia, voluptate unde accusamus delectus, tempore dolore odit laborum facilis quaerat velit? </p>
-    
-    </article>
-  </section>
+          </div>
+        </div>
+      </article>
 
-  <section class='partyInfo'>
-    <article class="partyInfo__item" id='policies'>
-      <h2>Educate yourself and vote based on what matters to you</h2>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem, sed tenetur harum quis totam excepturi porro qui beatae officia, voluptate unde accusamus delectus, tempore dolore odit laborum facilis quaerat velit? </p>
-    
-    </article>
-  </section>
+    </section>
+
+    <section class='partyInfo'>
+      <h1> Canada's Political Parties </h1>
+      <article class="partyInfo__item" id='history'>
+        <h2>The future of Canada depends on your vote</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem, sed tenetur harum quis totam excepturi porro qui beatae officia, voluptate unde accusamus delectus, tempore dolore odit laborum facilis quaerat velit? </p>
+      
+      </article>
+      <article class="partyInfo__item" id='policies'>
+        <h2>Educate yourself and vote based on what matters to you</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem, sed tenetur harum quis totam excepturi porro qui beatae officia, voluptate unde accusamus delectus, tempore dolore odit laborum facilis quaerat velit? </p>
+      
+      </article>
+    </section>
+</main>
 
 <?php include "partials/footer.php" ?>
