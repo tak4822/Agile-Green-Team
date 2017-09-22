070975c8491da1cb38556de4fa506a048202ca0b
tak4822 <tkc4822@gmail.com>
2017-09-22 09:08:20 -0700

    beta desktop

--- mp.php
+++ mp.php
@@ -9,24 +9,25 @@
       "Click the map to see MPs. Check MP’s in your province."
     );
   ?>
-  <div class="mpMap__provNav">
-    <ul>
-      <li class="mpMap__provNav__list">British Columbia</li>
-      <li class="mpMap__provNav__list">Alberta</li>
-      <li class="mpMap__provNav__list">Saskatchewan</li>
-      <li class="mpMap__provNav__list">Manitoba</li>
-      <li class="mpMap__provNav__list">Ontario</li>
-      <li class="mpMap__provNav__list">Quebec</li>
-      <li class="mpMap__provNav__list">New Brunswick</li>
-      <li class="mpMap__provNav__list">Nova Scotia</li>
-      <li class="mpMap__provNav__list">Prince Edward Island</li>
-      <li class="mpMap__provNav__list">Newfoudland and Labrador</li>
-      <li class="mpMap__provNav__list">Yukon</li>
-      <li class="mpMap__provNav__list">Northwest Territories</li>
-      <li class="mpMap__provNav__list">Nunavut</li>
-    </ul>
-  </div>
+
   <div class="mpMap__container">
+    <div class="mpMap__provNav">
+      <ul>
+        <li class="mpMap__provNav__list" data-prov="BC">British Columbia</li>
+        <li class="mpMap__provNav__list" data-prov="alberta">Alberta</li>
+        <li class="mpMap__provNav__list" data-prov="saskatchewan">Saskatchewan</li>
+        <li class="mpMap__provNav__list" data-prov="manitoba">Manitoba</li>
+        <li class="mpMap__provNav__list" data-prov="ontario">Ontario</li>
+        <li class="mpMap__provNav__list" data-prov="quebec">Quebec</li>
+        <li class="mpMap__provNav__list" data-prov="newBrunswick">New Brunswick</li>
+        <li class="mpMap__provNav__list" data-prov="novaScotia">Nova Scotia</li>
+        <li class="mpMap__provNav__list" data-prov="princeEdward">Prince Edward Island</li>
+        <li class="mpMap__provNav__list" data-prov="newfoundland">Newfoudland and Labrador</li>
+        <li class="mpMap__provNav__list" data-prov="yukon">Yukon</li>
+        <li class="mpMap__provNav__list" data-prov="northwest">Northwest Territories</li>
+        <li class="mpMap__provNav__list" data-prov="nunavut">Nunavut</li>
+      </ul>
+    </div>
     <?php include "partials/svg.php"; ?>
     <div class="mpMap__islands__container">
       <?php include "partials/sm-svg.php"; ?>
@@ -34,10 +35,11 @@
   </div>
   <div class="mpList__container">
 
-    <div class="mpList__party leberal">
+    <div class="mpList__party liberal">
       <h3 class="mpList__head">Liberal</h3>
       <ul class="mpList__list">
         <!-- put mps's name here  -->
+        
       </ul>
     </div>
 
