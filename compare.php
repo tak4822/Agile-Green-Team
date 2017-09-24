c97fe65b5ccdb8f24ea4f216d18721c7796478a2
tak4822 <tkc4822@gmail.com>
2017-09-23 20:20:51 -0700

    filter

--- compare.php
+++ compare.php
@@ -10,52 +10,102 @@
       "Click the policy which you like, and see your preference.<br> Use filter so that you could compare parties that you consider."
     );
   ?>
-    <section class='compare__filter'>
-        <!-- FILTER -->
-        <h2> Which parties would you like to compare? </h2>
-        <!--CONSERVATIVE -->
-        <label for='check--blue' class='party__label' id='blue'>Conservative</label>
-        <input type='checkbox' class='party__input' name='party' value='Conservative' id='check--blue'>
-        <!-- GREEN PARTY -->
-        <label for='check--green' class='party__label' id='green'>Liberal</label>
-        <input type='checkbox' class='party__input' name='party_input' value='Green' id='check--green'>
-        <!--LIBERAL PARTY -->
-        <label for='check--red' class='party__label' id='red'>Green</label>
-        <input type='checkbox' class='party__input' name='party_input' value='Liberal' id='check--red'>
-        <!--NDP PARTY -->
-        <label for='check--orange' class='party__label' id='orange'>NDP</label>
-        <input type='checkbox' class='party__input' name='party_input' value='NDP' id='check--orange'>  <br>
+    <!-- FILTER -->
+    <div class="compare__filter__wrap">
+      <section class='compare__filter'>
+          <div class="compare__filter__bigTitle">
+            <img src="img/filter.svg" alt="compare parties filter icon">
+            <h3>Filter</h2>
+          </div>
+          <!-- filter party -->
+          <div class="filter__bgWhite">
+            <h4 class="filter__title">Which <span class="green-strong">parties</span> would you like to compare? </h4>
+            <div class="filter__party">
+              <!--CONSERVATIVE -->
+              <label class="filter__checkWrap">
+                <input type="checkbox" name="party[]" value="blue" class="filter__input" checked>
+                <span class="filter__parts">Conservative</span>
+              </label>
+              <!-- GREEN PARTY -->
+              <label class="filter__checkWrap">
+                <input type="checkbox" name="party[]" value="green" class="filter__input" checked>
+                <span class="filter__parts">Green</span>
+              </label>
+              <!--LIBERAL PARTY -->
+              <label class="filter__checkWrap">
+                <input type="checkbox" name="party[]" value="red" class="filter__input" checked>
+                <span class="filter__parts">Liberal</span>
+              </label>
+              <!--NDP PARTY -->
+              <label class="filter__checkWrap">
+                <input type="checkbox" name="party[]" value="orange" class="filter__input" checked>
+                <span class="filter__parts">NDP</span>
+              </label>
+            </div>
 
-        <!--POLICY FILTER -->
-        <h2> Which policies would you like to compare? </h2>
-        <!--YOUTH-->
-        <label for='youth__input' class='policy__label' id='youth__label'>Youth </label>
-        <input type='checkbox' class='policy__input' name='policy' value='Youth' id='youth__input'>
-        <!--MARIJUANA-->
-        <label for='marijuana__input' class='policy__label' id='marijuana__label'>Marijuana </label>
-        <input type='checkbox' class='policy__input' name='policy' value='Marijuana' id='marijuana__input'>
-        <!--FIRST NATIONS-->
-        <label for='first_nations__input' class='policy__label' id='first_nations__label'>First Nations</label>
-        <input type='checkbox' class='policy__input' name='policy' value='First Nations' id='first_nations__input'>
-        <!--HOUSING-->
-        <label for='housing__input' class='policy__label' id='housing__label'>Housing</label>
-        <input type='checkbox' class='policy__input' name='policy' value='Housing' id='housing__input'>
-        <!--HEALTH-->
-        <label for='health__input' class='policy__label' id='health__label'>Health</label>
-        <input type='checkbox' class='policy__input' name='policy' value='Health' id='health__input'>
-        <!--ENVIRONMENT-->
-        <label for='environment__input' class='policy__label' id='environment__label'>Environment</label>
-        <input type='checkbox' class='policy__input' name='policy' value='Environment' id='environment__input'>
-        <!--PRIVACY-->
-        <label for='privacy__input' class='policy__label' id='privacy__label'>First Nations</label>
-        <input type='checkbox' class='policy__input' name='policy' value='privacy' id='privacy__input'>
-        <!--VETERAN-->
-        <label for='veteran__input' class='policy__label' id='veteran__label'>Veteran</label>
-        <input type='checkbox' class='policy__input' name='policy' value='Veteran' id='veteran__input'>
-        <!--PIPELINE -->
-        <label for='pipeline__input' class='policy__label' id='pipeline__label'>Pipeline</label>
-        <input type='checkbox' class='policy__input' name='policy' value='Pipeline' id='pipeline__input'>
-    </section>
+            <!--POLICY FILTER -->
+            <h4 class="filter__title"> Which <span class="green-strong">policies</span> would you like to compare? </h4>
+            <div class="filter__policies">
+              <!--YOUTH-->
+              <label class="filter__checkWrap">
+                <input type="checkbox" name="policies[]" value="youth" class="filter__input" checked>
+                <span class="filter__parts">Youth</span>
+              </label>
+
+              <!--MARIJUANA-->
+              <label class="filter__checkWrap">
+                <input type="checkbox" name="policies[]" value="marijuana" class="filter__input" checked>
+                <span class="filter__parts">Marijuana</span>
+              </label>
+
+              <!--FIRST NATIONS-->
+              <label class="filter__checkWrap">
+                <input type="checkbox" name="policies[]" value="firstNations" class="filter__input" checked>
+                <span class="filter__parts">First Nations</span>
+              </label>
+
+              <!--HOUSING-->
+              <label class="filter__checkWrap">
+                <input type="checkbox" name="policies[]" value="housing" class="filter__input" checked>
+                <span class="filter__parts">Housing</span>
+              </label>
+
+              <!--HEALTH-->
+              <label class="filter__checkWrap">
+                <input type="checkbox" name="policies[]" value="health" class="filter__input" checked>
+                <span class="filter__parts">Health</span>
+              </label>
+
+              <!--ENVIRONMENT-->
+              <label class="filter__checkWrap">
+                <input type="checkbox" name="policies[]" value="Enviroment" class="filter__input" checked>
+                <span class="filter__parts">Enviroment</span>
+              </label>
+
+              <!--PRIVACY-->
+              <label class="filter__checkWrap">
+                <input type="checkbox" name="policies[]" value="privacy" class="filter__input" checked>
+                <span class="filter__parts">Privacy</span>
+              </label>
+
+              <!--VETERAN-->
+              <label class="filter__checkWrap">
+                <input type="checkbox" name="policies[]" value="veteran" class="filter__input" checked>
+                <span class="filter__parts">Veteran</span>
+              </label>
+
+              <!--PIPELINE -->
+              <label class="filter__checkWrap">
+                <input type="checkbox" name="policies[]" value="pipeline" class="filter__input" checked>
+                <span class="filter__parts">Pipeline</span>
+              </label>
+            </div>
+
+          </div>
+
+      </section>
+    </div>
+
 
     <section class='compare__policies'>
         <article></article>
