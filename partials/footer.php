1ec7359963decf62ac2d3cbbc2b9721ff96a07fe
tak4822 <tkc4822@gmail.com>
2017-09-24 21:10:14 -0700

    fix bug from conflix

--- partials/footer.php
+++ partials/footer.php
@@ -1,29 +1,25 @@
 <div class="footer__stopper"></div>
 <footer class='footerContainer'>
-
-  <section class='footerContainer__form'>
-      <h3> Keep up to date, Subscribe to our newsletter </h3>
+  <div class="footer__wrapper">
+    <section class='footerContainer__nav'>
+      <a class="footerNav__list" href="index.php">Home</a>
+      <a class="footerNav__list" href="parties.php">Parties</a>
+      <a class="footerNav__list" href="services.php">Who's My MP</a>
+      <a class="footerNav__list" href="rates.php">Comparing Parties</a>
+    </section>
+    <section class='footerContainer__form'>
+      <h3 class="footer__form__title">NewsLetter</h3>
+      <p class="footer__form__desc">Keep up to date, Subscribe to our newsletter </p>
       <form action="<?php echo htmlentities($_SERVER['PHP_SELF'])?>" method='POST'>
-        <label for='nameInp'> Full Name 
-        <input type='text' name='fullName' id='nameInp' class='form-input'>
-        </label>
-        <label for='nameInp'> Email Address
-        <input type='email' name='email' id='emailInp' class='form-input'>
-        </label>
-        <input type='submit' value='Subscribe'>
-      </form>
-  </section> 
-  <section class='footerContainer__links'>
-    <h3> Contact Us </h3>
-    <p> vanarts@vanarts.com </p>
-    <p> 604-509-2093 </p>
-
+        <div class="fotter__form__field">
+          <label for='nameInp'>Name :</label>
+          <input type='text' name='fullName' id='nameInp' class='form-input' placeholder="Fill in your full name">
+        </div>
 
         <div class="fotter__form__field">
           <label for='nameInp'>Email :</label>
           <input type='email' name='email' id='emailInp' class='form-input' placeholder="Fill in your Email">
         </div>
-
 
         <input class="footer__forn__btn" type='submit' value='Subscribe'>
       </form>
@@ -46,7 +42,6 @@
 
   </div>
   <div class="copyRight-wrapper"><p class="copyRight">Vanarts Agile projects By Green Team</p></div>
-
 </footer>
 <script
   src="https://code.jquery.com/jquery-3.2.1.min.js"
@@ -54,7 +49,6 @@
   crossorigin="anonymous"></script>
   <script src="js/party.js"></script>
   <script src="js/mp.js"></script>
-  <script src="js/compare.js"></script>
   <script src='js/main.js'></script>
 </body>
 </html>
