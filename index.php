b55fc234304f220d88db6d87eb9756db4148b1bc
Coleman Jackson <burrard.bc@gmail.com>
2017-09-27 15:34:25 -0700

    home page style start

--- index.php
+++ index.php
@@ -1,36 +1,46 @@
-<?php include "partials/head.php" ?>
+<?php include "partials/head.php";?>
 <body>
-    <main>
-        <section>
-            <iframe id="player"></iframe>
-            <img src="" alt="">
+    <main class="home-page">
+        <?php
+        include "partials/header.php";
+        indexHeader(
+            "home"
+        );
+        ?>
+        <section class="landing">
+            <div class="video-background">
+                <div class="video-foreground">
+                    <iframe src="https://www.youtube.com/embed/7C8uDuhLqvg?rel=0&amp;controls=0&amp;showinfo=0" frameborder="0" allowfullscreen></iframe>
+                </div>
+            </div>
+
             <a href=""></a>
             <h1>Would you like to know what your party is</h1>
-            <a href="">Compare Parties</a>
+            <a href="compare.php">Compare Parties</a>
 
         </section>
-        <section>
-            <img src="" alt=""/>
+        <section class="sect-one">
+            <img src="img/vote.svg" alt=""/>
             <article>
                 <h2>comparing parties</h2>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
-                <a href="">See More</a>
+                <a class="btn" href="compare.php">See More</a>
             </article>
         </section>
-        <section>
+        <section class="sect-two">
             <article>
                 <h2>parties</h2>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
-                <a href="">See More</a>
+                <a class="btn" href="parties.php">See More</a>
             </article>
-            <img src="" alt=""/>
+            <img src="img/politics.svg" alt=""/>
         </section>
-        <section>
-            <img src="" alt=""/>
+        <section class="sect-three">
+            <img src="img/question.svg" alt=""/>
             <article>
                 <h2>who's my mp</h2>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
-                <a href="">See More</a>
+                <a class="btn" href="mp.php">See More</a>
             </article>
         </section>
     </main>
